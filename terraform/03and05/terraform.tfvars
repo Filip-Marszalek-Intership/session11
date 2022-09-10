3aee2d5b0d67fa7a4c93dd1135d4fdb5267ad65e
Marszalek <filip.marszalek@capgemini.com>
2022-09-10 21:22:56 +0200

    rename

---
+++ terraform/03and05/terraform.tfvars
@@ -0,0 +1,5 @@
+location = "uksouth"
+appServiceName = "appservice246"
+pricingTier = "B1"
+numOfNodes = 1
+dockerImage = "filip246/hello_world_nginx"
---
+++ terraform/03and05/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  type    = string
+}
+variable "appServiceName" {
+  type    = string
+}
+variable "pricingTier" {
+  type    = string
+}
+variable "numOfNodes" {
+  type    = number
+}
+variable "dockerImage" {
+  type    = string
+}
