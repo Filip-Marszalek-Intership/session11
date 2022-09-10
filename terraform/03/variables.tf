665ae9a22dc58d4fef4a200bbaa21890b83e4dcd
Marszalek <filip.marszalek@capgemini.com>
2022-09-10 21:20:16 +0200

    Reworked parameters in 03 using terraform.tfvars file

--- terraform/03/variables.tf
+++ terraform/03/variables.tf
@@ -1,20 +1,15 @@
 variable "location" {
   type    = string
-  default = "uksouth"
 }
 variable "appServiceName" {
   type    = string
-  default = "appservice246"
 }
 variable "pricingTier" {
   type    = string
-  default = "B1"
 }
 variable "numOfNodes" {
   type    = number
-  default = 1
 }
 variable "dockerImage" {
   type    = string
-  default = "filip246/hello_world_nginx"
 }
